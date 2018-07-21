c672477463b9db36174f4f9908aae7834d03dd0c
dhu0001 <16512386+dh0001@users.noreply.github.com>
2018-07-21 15:42:11 -0400

    Application starts up

---
+++ store/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using store.Data;
+using store.Models;
+using Microsoft.AspNetCore.Identity;
+using store.Models.AdminViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace store.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly RoleManager<ApplicationRole> roleManager;
+
+        public AdminController(RoleManager<ApplicationRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            List<RoleListViewModel> model = new List<RoleListViewModel>();
+            model = roleManager.Roles.Select(r => new RoleListViewModel
+            {
+                RoleName = r.Name,
+                Id = r.Id,
+                Description = r.Description,
+                //NumberOfUsers = r.Users.Count
+            }).ToList();
+            return View(model);
+        }
+    }
+}
---
+++ store/Models/ProductModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace store.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Image> Images { set; get; }
+        public ICollection<Category> Cetegories { set; get; }
+    }
+
+    public class Image
+    {
+        public int Id { set; get; }
+        public byte[] ImageData { set; get; }
+        public int ProductId { set; get; }
+    }
+
+    public class Category
+    {
+        public int Id { set; get; }
+        public int ProductId { set; get; }
+        public string CategoryName { set; get; }
+    }
+}
